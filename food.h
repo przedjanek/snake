91918e3ff724ce53bc0e3788b349d448b344ce3f
przedjanek <76000073+przedjanek@users.noreply.github.com>
2021-01-17 23:49:17 +0100

    Create food.h

---
+++ food.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include"Snake.h"
+#include "Board.h"
+#include <random>
+
+
+
+class food
+{
+public:
+	food(std::mt19937& rng, const Board& brd, const Snake& snake);
+	void Respawn(std::mt19937& rng, const Board& brd, const Snake& snake);
+	void Draw(Board& brd) const;
+	const Location& GetLocation() const;
+
+
+private:
+	static constexpr Color c = Colors::Red;
+	Location loc;
+
+		
+
+
+
+};
+
