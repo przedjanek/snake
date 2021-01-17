a86507ab3c7f510c5093b1958d44a01ab7a8bab0
przedjanek <76000073+przedjanek@users.noreply.github.com>
2021-01-17 23:49:37 +0100

    Create food.cpp

---
+++ food.cpp
@@ -0,0 +1,33 @@
+#include "food.h"
+
+food::food(std::mt19937& rng, const Board& brd, const Snake& snake)
+{
+	Respawn(rng, brd, snake);
+}
+
+void food::Respawn(std::mt19937& rng, const Board& brd, const Snake& snake)
+{
+	std::uniform_int_distribution<int> xDist(0, brd.GetGridWidth() - 1);
+	std::uniform_int_distribution<int> yDist(0, brd.GetGridHeight() - 1);
+
+	Location newLoc;
+	do
+	{
+		newLoc.x = xDist(rng);
+		newLoc.y = yDist(rng);
+	} while (snake.IsInTile(newLoc));
+
+	loc = newLoc;
+}
+
+void food::Draw(Board& brd) const
+{
+	brd.DrawCell(loc, c);
+
+
+}
+
+const Location& food::GetLocation() const
+{
+	return loc;
+}
